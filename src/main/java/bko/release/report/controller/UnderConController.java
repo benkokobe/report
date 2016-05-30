0f6884a3964cd719495c8adf8a35316bcf550a67
bkokobe <ben.kokobe@gmail.com>
2016-05-30 20:02:23 +0200

    commit from eclipse: 30052016

--- src/main/java/bko/release/report/controller/UnderConController.java
+++ src/main/java/bko/release/report/controller/UnderConController.java
@@ -2,6 +2,8 @@ package bko.release.report.controller;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/under_con")
 public class UnderConController {
+	private static final Logger logger = LoggerFactory.getLogger(UnderConController.class);
 	@Value("${env.name}")
 	private String env_name;
 
@@ -18,6 +21,7 @@ public class UnderConController {
 	public String home(Locale locale, Model model) {
 
 		model.addAttribute("env_name", env_name );
+		logger.info("under con controller");
 
 		return "under_con";
 	}
